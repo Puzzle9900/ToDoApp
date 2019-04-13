30ef16a86bc0f98ae8fa2b62de51ffd13b7db3bd
Gabriel <puzzle9900@gmail.com>
2019-04-13 17:00:07 -0400

    Installed normalize.css

--- src/index.js
+++ src/index.js
@@ -2,22 +2,15 @@ import React from 'react'
 import {render } from 'react-dom';
 import configureStore from 'redux-config'
 import { Provider } from 'react-redux'
-import './index.css';
 import * as serviceWorker from './serviceWorker';
-// import { BrowserRouter as Router } from 'react-router-dom'
-// import App from 'components/App'
-
-import AppRouter from 'components/AppRouter'
-
+import './index.css';
+import 'normalize.css';
 
 const store = configureStore()
 
 render(
     <Provider store={store}>
-        {/* <Router>
-            <App />3
-        </Router> */}
-        <AppRouter/>
+        <input value={'hello'} />
     </Provider>
     , document.getElementById('root'));
 
